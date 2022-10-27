410b71358c23634a5e903f464a60dd070ee405d5
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2022-10-27 15:37:08 +0700

    second commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './about.css';
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+
+      <div className='container about __container'>
+        <div className="about_me">
+          <div className="about__me-image">
+            <img src="" alt="" />
+          </div>
+        </div>
+
+        <div className="about__content"></div>
+      </div>
+    </section>
+  )
+}
+
+export default About
